d33aafdca5b2756e592312290d91f01ffb75e923
George Blackmore <gtblackmore@gmail.com>
2019-12-17 03:52:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
-dennis_ritchies_language[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages] 
+
 end
 
 def adding_matz
