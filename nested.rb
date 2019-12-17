73901018bfdc8ab281847b28481deb5dfdf34806
George Blackmore <gtblackmore@gmail.com>
2019-12-17 04:09:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -82,8 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+ programmer_hash  
 end
 
 def changing_alan
